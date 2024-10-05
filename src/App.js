a070474a53808955c3377bb3853a6e7861f03e50
spallisripple <sotiris.pallis@ripplehealthgroup.com>
2024-10-05 18:48:16 +0300

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
 import LockiePrivacyPolicyPage from './LockiePrivacyPolicyPage';
 import RhythmixPrivacyPolicyPage from './RhythmixPrivacyPolicyPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi I'm Sotiris Pallis</h1>
-      <p>This is my site, but it's still under construction...</p>
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route path="/lockie/privacy" element={<LockiePrivacyPolicyPage />} />
         <Route path="/rhythmix/privacy" element={<RhythmixPrivacyPolicyPage />} />
-      </Routes> */}
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
